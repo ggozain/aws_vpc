e08c2309f99f9a80324cfbc9f645ea7230e25443
Gerardo Gozain <gozain@gmail.com>
2023-04-17 17:19:24 +0100

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs             = tolist(data.aws_availability_zones.available[*].names)
+  azs             = values(data.aws_availability_zones.available[*].names)
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
