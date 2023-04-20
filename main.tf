a2fb98706257273ab8f7abe9a65ca62b03f047a1
Gerardo Gozain <gozain@gmail.com>
2023-04-20 12:54:28 +0100

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -15,16 +15,16 @@ module "vpc" {
   enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
   use_ipam_pool        = var.use_ipam_pool
-  ipv4_ipam_pool_id    = data.tfe_outputs.ipam.values.ipam_id
+  ipv4_ipam_pool_id    = data.tfe_outputs.ipam.values.ipam_parent_pool_id
   ipv4_netmask_length  = var.ipv4_netmask_length
 
   azs             = var.azs
-  private_subnets = var.private_subnets
+  # private_subnets = var.private_subnets
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.expected_eks_cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"                        = 1
   }
-  public_subnets = var.public_subnets
+  # public_subnets = var.public_subnets
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.expected_eks_cluster_name}" = "shared"
     "kubernetes.io/role/elb"                                 = 1
