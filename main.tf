20ae6043a2f6530781c9a4e173d9d8a94c9f1c4c
Gerardo Gozain <gozain@gmail.com>
2023-04-25 11:02:47 +0100

    format changes#

--- main.tf
+++ main.tf
@@ -14,36 +14,36 @@ locals {
 
 
 resource "aws_vpc_ipam_preview_next_cidr" "this" {
-  ipam_pool_id          = data.tfe_outputs.ipam.values.ipam_parent_pool_id
-  netmask_length        = var.ipv4_netmask_length
+  ipam_pool_id   = data.tfe_outputs.ipam.values.ipam_parent_pool_id
+  netmask_length = var.ipv4_netmask_length
 }
 
 module "vpc" {
-  source                = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-  tags                  = {
+  tags = {
     "Project"     = var.project_name
     "Environment" = var.infra_env
   }
-  name                  = "${var.infra_env}-${var.aws_region}-vpc"
-  enable_nat_gateway    = var.enable_nat_gateway
-  single_nat_gateway    = var.single_nat_gateway
-  one_nat_gateway_per_az = var.one_nat_gateway_per_az 
-  enable_dns_support    = var.enable_dns_support
-  enable_dns_hostnames  = var.enable_dns_hostnames
-  use_ipam_pool         = var.use_ipam_pool
-  ipv4_ipam_pool_id     = data.tfe_outputs.ipam.values.ipam_parent_pool_id
-  ipv4_netmask_length   = var.ipv4_netmask_length
-  cidr                  = data.tfe_outputs.ipam.values.ipam_parent_pool_cidr
+  name                   = "${var.infra_env}-${var.aws_region}-vpc"
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
+  enable_dns_support     = var.enable_dns_support
+  enable_dns_hostnames   = var.enable_dns_hostnames
+  use_ipam_pool          = var.use_ipam_pool
+  ipv4_ipam_pool_id      = data.tfe_outputs.ipam.values.ipam_parent_pool_id
+  ipv4_netmask_length    = var.ipv4_netmask_length
+  cidr                   = data.tfe_outputs.ipam.values.ipam_parent_pool_cidr
 
-  azs                   = local.azs
-  private_subnets       = cidrsubnets(local.preview_partition[0], 2, 2, 2)
-  public_subnets        = cidrsubnets(local.preview_partition[1], 2, 2, 2)
-  private_subnet_tags   = {
+  azs             = local.azs
+  private_subnets = cidrsubnets(local.preview_partition[0], 2, 2, 2)
+  public_subnets  = cidrsubnets(local.preview_partition[1], 2, 2, 2)
+  private_subnet_tags = {
     "kubernetes.io/cluster/${var.expected_eks_cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"                        = 1
   }
-  public_subnet_tags    = {
+  public_subnet_tags = {
     "kubernetes.io/cluster/${var.expected_eks_cluster_name}" = "shared"
     "kubernetes.io/role/elb"                                 = 1
   }
