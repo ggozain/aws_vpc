92f5b94512ac3a674baf3f7a7a2a6f88d9e9ef7f
Gerardo Gozain <gozain@gmail.com>
2023-04-25 11:48:12 +0100

    more changes

--- main.tf
+++ main.tf
@@ -5,18 +5,20 @@ data "tfe_outputs" "ipam" {
 
 data "aws_availability_zones" "available" {}
 
-locals {
-
-  preview_partition = cidrsubnets(data.tfe_outputs.ipam.values.ipam_parent_pool_cidr, 2, 2, 2)
-  azs               = slice(data.aws_availability_zones.available.names, 0, 3)
-
-}
-
-
-resource "aws_vpc_ipam_preview_next_cidr" "this" {
-  ipam_pool_id   = data.tfe_outputs.ipam.values.ipam_parent_pool_id
+data "aws_vpc_ipam_preview_next_cidr" "previewed_cidr" {
+  ipam_pool_id   = data.tfe_outputs.ipam.values.ipam_child_pool_id
   netmask_length = var.ipv4_netmask_length
 }
+
+locals {
+
+  partition       = cidrsubnets(data.aws_vpc_ipam_preview_next_cidr.previewed_cidr.cidr, 2, 2)
+  private_subnets = cidrsubnets(local.partition[0], 2, 2)
+  public_subnets  = cidrsubnets(local.partition[1], 2, 2)
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+
+}
+
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -32,13 +34,13 @@ module "vpc" {
   enable_dns_support     = var.enable_dns_support
   enable_dns_hostnames   = var.enable_dns_hostnames
   use_ipam_pool          = var.use_ipam_pool
-  ipv4_ipam_pool_id      = data.tfe_outputs.ipam.values.ipam_parent_pool_id
-  ipv4_netmask_length    = var.ipv4_netmask_length
-  cidr                   = data.tfe_outputs.ipam.values.ipam_parent_pool_cidr
+  ipv4_ipam_pool_id      = data.tfe_outputs.ipam.values.ipam_child_pool_id
+  ipv4_netmask_length    = data.tfe_outputs.ipam.values.ipam_child_pool_netmask_length
+  cidr                   = data.tfe_outputs.ipam.values.ipam_child_pool_cidr
 
   azs             = local.azs
-  private_subnets = cidrsubnets(local.preview_partition[0], 2, 2, 2)
-  public_subnets  = cidrsubnets(local.preview_partition[1], 2, 2, 2)
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.expected_eks_cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"                        = 1
