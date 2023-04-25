a41c8671d8f24f3eda0c56761c37f012f0df143f
Gerardo Gozain <gozain@gmail.com>
2023-04-25 15:49:20 +0100

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "aws_vpc_ipam_preview_next_cidr" "previewed_cidr" {
 
 locals {
 
-  partition       = cidrsubnets(data.aws_vpc_ipam_preview_next_cidr.previewed_cidr.cidr, 0, 0)
+  partition       = cidrsubnets(data.aws_vpc_ipam_preview_next_cidr.previewed_cidr.cidr, 2, 2)
   private_subnets = cidrsubnets(local.partition[0], 2, 2, 2)
   public_subnets  = cidrsubnets(local.partition[1], 2, 2, 2)
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
