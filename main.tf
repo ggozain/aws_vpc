12fa5b1a8ed577564f26932b65103e07024ab31a
Gerardo Gozain <gozain@gmail.com>
2023-04-26 09:56:01 +0100

    changed subnet calculations to be linked to number of azs

--- main.tf
+++ main.tf
@@ -13,10 +13,11 @@ data "aws_vpc_ipam_preview_next_cidr" "previewed_cidr" {
 locals {
 
   partition       = cidrsubnets(data.aws_vpc_ipam_preview_next_cidr.previewed_cidr.cidr, 2, 2)
-  private_subnets = cidrsubnets(local.partition[0], 2, 2, 2)
-  public_subnets  = cidrsubnets(local.partition[1], 2, 2, 2)
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
-
+  private_subnets = [for i, n in local.azs : cidrsubnet(local.partition[0], 4, (i))]
+  # cidrsubnets(local.partition[0], 2, 2, 2)
+  public_subnets = [for i, n in local.azs : cidrsubnet(local.partition[1], 4, (i))]
+  # cidrsubnets(local.partition[1], 2, 2, 2)
 }
 
 
