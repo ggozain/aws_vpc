aa1f7716b9fc2806fda12efd16c175ff1fc58edb
Gerardo Gozain <gozain@gmail.com>
2023-04-27 11:57:00 +0100

    added karpenter tags and intrea subnets. Enabled cloudwatch logs

--- main.tf
+++ main.tf
@@ -12,12 +12,15 @@ data "aws_vpc_ipam_preview_next_cidr" "previewed_cidr" {
 
 locals {
 
-  partition       = cidrsubnets(data.aws_vpc_ipam_preview_next_cidr.previewed_cidr.cidr, 2, 2)
+  partition       = cidrsubnets(data.aws_vpc_ipam_preview_next_cidr.previewed_cidr.cidr, 2, 2, 2)
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   private_subnets = [for i, n in local.azs : cidrsubnet(local.partition[0], 4, (i))]
   # cidrsubnets(local.partition[0], 2, 2, 2)
   public_subnets = [for i, n in local.azs : cidrsubnet(local.partition[1], 4, (i))]
   # cidrsubnets(local.partition[1], 2, 2, 2)
+  intra_subnets = [for i, n in local.azs : cidrsubnet(local.partition[2], 4, (i))]
+  # cidrsubnets(local.partition[1], 2, 2, 2)
+  name            = "${var.infra_env}-${var.aws_region}-vpc"
 }
 
 
@@ -28,7 +31,7 @@ module "vpc" {
     "Project"     = var.project_name
     "Environment" = var.infra_env
   }
-  name                   = "${var.infra_env}-${var.aws_region}-vpc"
+  name                   = local.name
   enable_nat_gateway     = var.enable_nat_gateway
   single_nat_gateway     = var.single_nat_gateway
   one_nat_gateway_per_az = var.one_nat_gateway_per_az
@@ -37,11 +40,16 @@ module "vpc" {
   use_ipam_pool          = var.use_ipam_pool
   ipv4_ipam_pool_id      = data.tfe_outputs.ipam.values.ipam_child_pool_id
   ipv4_netmask_length    = var.ipv4_netmask_length
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_iam_role  = true
+  create_flow_log_cloudwatch_log_group = true
 
 
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.expected_eks_cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"                        = 1
@@ -49,5 +57,6 @@ module "vpc" {
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.expected_eks_cluster_name}" = "shared"
     "kubernetes.io/role/elb"                                 = 1
+    "karpenter.sh/discovery" = local.name
   }
 }
